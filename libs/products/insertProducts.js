cb1cf0ae57b7a60991f5225742344ce4b4e336e2
Matt England <m.p.england@gmail.com>
2017-10-29 20:05:56 +0000

    Before async map

--- libs/products/insertProducts.js
+++ libs/products/insertProducts.js
@@ -1,4 +1,5 @@
 // expressCart stuff.
+require('dotenv').config()
 var mongodb = require('mongodb');
 var async = require('async');
 var path = require('path');
@@ -40,11 +41,6 @@ module.exports.insertProducts = function(tweets, callback) {
 
             var opts = '{"Poster Colour":{"optName":"Poster Colour","optLabel":"Poster Colour","optType":"select","optOptions":["blue","red","white"]},"Frame":{"optName":"Frame","optLabel":"Framed","optType":"select","optOptions":["Yes","No"]}}'
             var insertThis = false;
-
-            // TODO: multiple product images, one is main
-            // TODO: options changed, show diff image?
-            // TODO: Sync with Printful API. ******************
-
             var tweetUnix = moment(tweet.created_at, 'ddd MMM DD HH:mm:ss Z YYYY');
             var monthYear = tweetUnix.format('MMMM YYYY');
             var tweetID = tweet.tweet_id.toString();
@@ -82,13 +78,13 @@ module.exports.insertProducts = function(tweets, callback) {
                             // Get the new document ID.
                             var newId = newDoc.ops[0]._id.toString();
 
-                            setS3ProductThumbs(tweetID, newId, function(err3) {
+                            setS3ProductThumbs(tweetID, newId, function(err3, newThumb) {
 
-                                // updateMainImg(db, tweetID, newId, function(err4) {
+                                updateMainImg(db, newThumb, function(err4) {
 
-                                    // if(err4) console.log(err4);
+                                    if(err4) console.log(err4);
                                     cb();
-                                // });
+                                });
                             });
                         }
                     });
@@ -136,7 +132,7 @@ module.exports.insertProducts = function(tweets, callback) {
     });
 }
 
-function updateMainImg(db, tweetID, docID, cb) {
+function updateMainImg(db, newThumb, cb) {
 
     // TODO: Save *just* the filename? we can then use env vars to change the path.
     // TODO: Set up a CNAME for posters.trumptweetposters.com and point it to s3 bucket
@@ -166,26 +162,24 @@ function updateMainImg(db, tweetID, docID, cb) {
     });
 }
 
+
+// We have thumbs stored in the root of the bucket. Those need to be moved
+// into a folder named by the new docID. Then, we need to save that path into
+// the DB. Don't save the full URL *with* the s3 url. we just want to keep
+// /docID/thumb.png
+
+// TODO: MOVE the THREE thumbs into that folder. SAVE just one filename to the DB.
+
+// TODO: Set up a CNAME for posters.trumptweetposters.com and point it to s3 bucket
+// TODO: Set up a CNAME for media.trumptweetposters.com and point it to s3 thumb bucket
 function setS3ProductThumbs(tweetID, docID, cb) {
-
-    // TODO: We have thumbs stored in the root of the bucket. Those need to be moved
-    // TODO: into a folder named by the new docID. Then, we need to save that path into
-    // TODO: the DB. Don't save the full URL *with* the s3 url. we just want to keep
-    // TODO: /docID/thumb.png
-
-    // TODO: MOVE the THREE thumbs into that folder. SAVE just one filename to the DB.
-
-    // TODO: Set up a CNAME for posters.trumptweetposters.com and point it to s3 bucket
-    // TODO: Set up a CNAME for media.trumptweetposters.com and point it to s3 thumb bucket
-
-
 
     // If any error is passed to a successive function, the waterfall goes 
     // straight to it's top-level callback.
     async.waterfall([
         function list(next) {
 
-            console.log('wait...');
+            console.log('wait for thumbnail creation by Lambda function.');
 
             // The generated thumbs aren't available for a good few seconds. Wait for 
             // lambda to do it's thing.
@@ -215,209 +209,128 @@ function setS3ProductThumbs(tweetID, docID, cb) {
         },
         function download(files, next) {
 
-            console.log('-----');
-            console.log('download');
-            console.log(files);
-            console.log();
-            // next(null, files);
-
-            debugger;
-
             var i = 0;
+            var newThumbs = [];
 
-            async.eachSeries(files, function(thumb, callback) {
+            // async.map PUT HERE.
+            // Square each number in the array [1, 2, 3, 4]
+            async.map(files, square, function (err, results) {
+              // Square has been called on each of the numbers
+              // so we're now done!
+              console.log("Finished!");
+              console.log(results);
+            });
 
-                debugger;
+            async.eachSeries(files, function(thumb, callback2) {
 
                 var filenameOrig = tweetID + '--' + i + '.png';
                 var fullpath = path.join(__dirname, '../posters/poster-imgs/' + filenameOrig);
-                var file = fs.createWriteStream(fullpath);
                 var filename = docID + '/' + thumb;
+                newThumbs.push(filename);
 
+                console.log('----');
+                console.log(newThumbs);
+                console.log('----');
 
-                file.on('open', function() {
+                const s3Stream = s3.getObject({Bucket:S3_THUMBS, Key: files[i]}).createReadStream();
+                const fileStream = fs.createWriteStream(fullpath);
+                s3Stream.on('error', function() {
 
-                    console.log('open local file: ' + fullpath);
-                    debugger;
+                });
+                fileStream.on('error', function() {
 
-                    var s3Stream = s3.getObject({Bucket:S3_THUMBS, Key: files[i]}).createReadStream()
-                    .on('error', function(err) {
-                      console.log('READSTREAM error');
-                      console.log(err);
-                    })
-                    .on('end', function() {
+                });
+                // This is what gets called when the object has been written locally with pipe().
+                fileStream.on('close', () => {
+                    // console.log('Created local file: ' + filename + ' successfully.');
+                });
 
-                        console.log('open s3 stream')
+                s3Stream.pipe(fileStream).on('close', function() {
+
+                    const fileBuffer = fs.createReadStream(fullpath);
+
+                    // Upload back to s3 with new path.
+                    s3.putObject({
+                      Bucket: S3_THUMBS,
+                      ACL: 'public-read',
+                      Key: filename,
+                      Body: fileBuffer,
+                      ContentType: 'image/png'
+                    }, 
+                    function(err2, data) {
+                        if(err2) console.log(err2);
+                        console.log('Re-uploaded file: ' + filename + ' to s3 successfully.');
+                        newThumbs.push(fullUrl);
+
+                        if(i != files.length) {
+                            var fullUrl = 'https://' + S3_THUMBS + '.s3.amazonaws.com/' + filename;
+                            i++;
+                        }
+                        
                         debugger;
-
-                        s3Stream.pipe(file).on('close', function() {
-
-                            debugger;
-
-                            // Upload back to s3 with new path.
-                            s3.putObject({
-                              Bucket: S3_THUMBS,
-                              ACL: 'public-read',
-                              Key: filename,
-                              Body: file,
-                              ContentType: 'image/png'
-                            }, (err) => {
-
-                                debugger;
-                                if (err) {
-                                    console.log('error re-uploading to s3:')
-                                    return callback(err);
-                                } else {
-                                    console.log('Re-uploaded: ' + filename + ' to s3 successfully.')
-                                    debugger;
-                                    if(i == files.length) {
-                                        next(null, filenames);
-                                        i++;
-                                    }
-                                    else callback();
-                                }
-                            });
-                        });
+                        callback2(null, newThumbs);
                     });
                 });
 
-            }, function(err) {
+            }, function(err, newThumbs) {
+
+                console.log('Finished renaming thumbs. newThumbs: ');
+                console.log(newThumbs);
+                debugger;
+
+                if(err) console.log(err);
+                next(files, newThumbs);
+            });
+
+        },
+        function del(files, newThumbs, next) {
+
+            console.log(files);
+            console.log('deleting original thumbs..');
+            console.log();
+
+            var i = 1;
+
+            async.eachSeries(files, function(thumb, callback3) {
 
                 debugger;
 
-                console.log('ended.');
-                if(err) console.log(err);
-                next(null, filenames);
-            });
-
-
-            // async.eachSeries(Object.keys(config), function (key, next){ 
-            //   search(config[key].query, function(err, result) { // <----- I added an err here
-            //     if (err) return next(err)  // <---- don't keep going if there was an error
-
-            //     var json = JSON.stringify({
-            //       "result": result
-            //     });
-            //     results[key] = {
-            //       "result": result
-            //     }
-            //     next()    /* <---- critical piece.  This is how the forEach knows to continue to
-            //                        the next loop.  Must be called inside search's callback so that
-            //                        it doesn't loop prematurely.*/
-            //   })
-            // }, function(err) {
-            //   console.log('iterating done');
-            // }); 
-
-
-            // for (var i = 0; i < files.length; i++) {
-
-            //     console.log('file: ' + files[i]);
-
-            //     var filenameOrig = tweetID + '--' + i + '.png';
-            //     var fullpath = path.join(__dirname, '/poster-imgs/' + filenameOrig);
-            //     var file = fs.createWriteStream(fullpath);
-            //     var filename = docID + '/' + files[i];
-
-            //     console.log('new path: ' + filename);
-
-            //     file.on('close', function() {
-
-            //         console.log('file close');
-
-            //         checkIfFile(filePath, function(err, isFile) {
-
-            //             console.log('is file: ' + isFile);
-
-            //             if(isFile) {
-
-            //                 console.log('created temp local file: ' + filePath);
-
-            //                 // Download the image from S3 into a temp local file.
-            //                 s3.getObject({
-            //                     Bucket: S3_THUMBS,
-            //                     Key: files[i]
-            //                 }).createReadStream().on('error', function(err){
-            //                     console.log('error reading:');
-            //                     console.log(err);
-            //                 }).pipe(file);
-
-            //                 console.log('after getting object:')
-
-            //                 console.log(file);
-
-            //                 // Upload back to s3 with new path.
-            //                 s3.putObject({
-            //                   Bucket: S3_THUMBS,
-            //                   ACL: 'public-read',
-            //                   Key: filename,
-            //                   Body: file,
-            //                   ContentType: 'image/png',
-            //                 }, (err) => {
-            //                   if (err) {
-            //                     console.log('error re-uploading to s3:')
-            //                     next(err);
-            //                   } else {
-            //                     console.log('Re-uploaded: ' + filename + ' to s3 successfully.')
-            //                     if(i == files.length) next(null, filenames);
-            //                   }
-            //                 });
-
-            //             }
-            //             else {
-
-            //                 console.log('File does not exist: ' + filePath);
-            //             }
-
-
-            //         });
-            //     });
-
-            // };
-        },
-        function del(files, next) {
-            console.log('-----');
-            console.log('delete');
-            console.log(files);
-            console.log();
-
-            for (var i = 0; i < files.length; i++) {
-
-                console.log('deleting original thumb: ' + files[i]);
-
                 // Delete the existing object.
-                s3.deleteObject({Bucket:S3_THUMBS, Key: filename}, function(err, data) {
-                   if (err) next(err); // an error occurred
-                   console.log('Deleted original: ' + filename);
-                   if(i == files.length) {
-                        console.log('Finished deleting.');
-                        next(null);
-                   }
+                s3.deleteObject({Bucket:S3_THUMBS, Key: thumb}, function(err, data) {
+                    if (err) next(err); 
+
+                    if(i == files.length) {
+                        var rand = randomIntFromInterval(1, files.length);
+                        newThumb = files[rand];
+                        console.log('Choose random thumb for database: ' + newThumb);
+                        next(null, newThumb);
+                    }
+                    else {
+                        i++;
+                        callback3();
+                    }
                 });
-            }
-
-            next(null);
+            });
         }
-        ], function (err, result) {
-
-            console.log('Finished waterfall, calling back');
+        ], function (err, newThumb) {
 
             if (err) {
                 console.log(err);
             } else {
-                console.log('proper success mind');
-                // console.log(
-                //     'Successfully resized ' + tweetID + '/' + srcKey +
-                //     ' and uploaded to ' + dstBucket + '/' + dstKey
-                // );
+                console.log('Renamed thumbs successfully. Save this one: ' + newThumb);
+                cb(null, newThumb);
             }
-
-            cb();
         }
     );
+}
 
+function renameThumb(origThumb, docID, cb) {
 
+    
+}
 
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 function getSavedThumbs(tweetID, docID, single, cb) {
@@ -495,72 +408,3 @@ function getGeneratedThumbs(tweetID, docID, callback) {
         return callback(fileList);
     });
 }
-
-function moveThumbs(thumbs, cb) {
-
-    if(!thumbs || thumbs.length < 1) return cb('thumbs was not defined');
-
-    async.each(thumbs, function(thumb, callback) {
-
-        var docIdStr = thumb.docID.toString();
-        var to = path.join(__dirname, '../../public/uploads', docIdStr, thumb.filename);
-        var from = thumb.path;
-
-        // Check thumb is ready
-        thumbReady(from, function(success){
-
-            if(success) {
-
-                // Copy file
-                fs.move(from, to, { overwrite: true }, err => {
-                    if (err) return console.error(err)
-                    else {
-                        callback(null);
-                    }
-                })
-            }
-            else {
-                console.log('THERE WAS A PROBLEM MOVING: ' + from);
-            }
-        });
-
-    }, function(er) {
-
-        if(er) {
-            console.log('An error happened while moving thumbs');
-            cb(er);
-        } else {
-            console.log('Product thumbs moved successfully');
-            cb(null);
-        }
-    });
-}
-
-function checkIfFile(file, cb) {
-  fs.stat(file, function fsStat(err, stats) {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        return cb(null, false);
-      } else {
-        return cb(err);
-      }
-    }
-    return cb(null, stats.isFile());
-  });
-}
-
-
-function thumbReady(path, cb) {
-
-    fs.stat(path, function(err, stat) {
-        if(err == null) {
-            return cb(true);
-        } else if(err.code == 'ENOENT') {
-            return cb(false);
-        } else {
-            console.log('Some other error: ', err.code);
-            return cb(false);
-        }
-    });
-}
-
