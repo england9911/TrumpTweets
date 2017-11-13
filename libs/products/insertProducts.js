cc9528bfc4409857f37399f8678aea9b8c50e864
Matt England <m.p.england@gmail.com>
2017-11-13 22:11:10 +0000

    More useful comments

--- libs/products/insertProducts.js
+++ libs/products/insertProducts.js
@@ -28,11 +28,29 @@ module.exports.printfulOrder = function(req, order, callback) {
     // Ref: /routes/paypal.js
 
     // TODO: Send order to Printful.
-    // 24x36 matte poster = ID:2
+    // 24x36 matte poster unframed = ID:2
+    // 24x36 matte poster framed = ID: 4
+    // All variant IDs: https://www.printful.com/products    
     // info stored in: req.session
     // https://www.printful.com/docs/orders
+    // --
+    // Order info is also saved in DB by this point and will include:
+    // orderPaymentId: payment.id,
+    // orderPaymentGateway: 'Paypal',
+    // orderTotal: req.session.totalCartAmount,
+    // orderEmail: req.body.shipEmail,
+    // orderFirstname: req.body.shipFirstname,
+    // orderLastname: req.body.shipLastname,
+    // orderAddr1: req.body.shipAddr1,
+    // orderAddr2: req.body.shipAddr2,
+    // orderCountry: req.body.shipCountry,
+    // orderState: req.body.shipState,
+    // orderPostcode: req.body.shipPostcode,
+    // orderPhoneNumber: req.body.shipPhoneNumber,
+    // orderStatus: payment.state,
+    // orderDate: new Date(),
+    // orderProducts: req.session.cart
 
-    //
     // POST to: https://api.printful.com/orders?confirm=1
     // Skip the draft phase, go straight to confirmed order to be fulfilled.
     //
@@ -41,6 +59,8 @@ module.exports.printfulOrder = function(req, order, callback) {
     // and tweet ID then we can easily find the file.
 
     // TODO: ***** Get a successful snadbox payment callback to start testing this. ***
+
+
 
     console.log('REQ:');
     console.log(req);
