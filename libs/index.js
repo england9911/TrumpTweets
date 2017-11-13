cea33d8165fb2ca0fb8dc75951e73fe7561deac5
Matt England <m.p.england@gmail.com>
2017-11-13 20:52:11 +0000

    Removed emoji font

--- libs/index.js
+++ libs/index.js
@@ -3,35 +3,6 @@ var posters = require('./posters/makePoster');
 var products = require('./products/insertProducts');
 
 
-// var AWS = require('aws-sdk');
-// const S3_BUCKET = process.env.S3_BUCKET_NAME;
-// const S3_THUMBS = process.env.S3_THUMBS;
-// const s3 = new AWS.S3();
-
-// s3.listObjects({
-//     Bucket: S3_THUMBS,
-//     Delimiter: "/"
-// }, function(err, data) {
-
-
-//     if(err) console.log(err);
-//     else if (data.Contents.length === 0) {
-//         console.log('NO IMAGES RETURNED!');
-//     }
-
-//     console.log();
-
-//     var bucketContents = data.Contents;
-//     var rootFiles = [];
-//     for (var i = 0; i < bucketContents.length; i++) {
-//         rootFiles.push(bucketContents[i].Key);
-//     }
-
-//     console.log('root files:');
-//     console.log(rootFiles);
-// });
-
-
 // TODO: 	Upload generated posters to S3 bucket 'trumptweetposters'
 
 // TODO: 	Heroku scheduled job for the poster gen. Twitter webhook? (zapier.com enables this, posts to a URL)
@@ -39,9 +10,6 @@ var products = require('./products/insertProducts');
 // If you need a scheduled job < 10 min interval, you'll have to use something other than the basic scheduler add-on.
 
 // TODO: 	Environment vars: change mongoDB URI, poster folder location for LIVE app.
-
-
-// 	AWS s3 info stored as Heroku config vars: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, S3_BUCKET_NAME
 
 
 // TODO: Move libs confs to heroku vars.
@@ -53,46 +21,6 @@ var products = require('./products/insertProducts');
 // TODO: 	Doesn't look like you need to create individual 'products' saved on Printful. Only orders.
 //			https://www.printful.com/docs/orders
 
-
-/*
-
- Example request body for creating a new order, see: https://www.printful.com/docs/orders under
- 'Create order with external ID, custom item names and retail price information.'
-
-{
-    "external_id": "9887112",
-    "recipient": {
-        "name": "John Doe",
-        "address1": "19749 Dearborn St",
-        "city": "Chatsworth",
-        "state_code": "CA",
-        "country_code": "US",
-        "zip": "91311"
-    },
-    "items": [{
-        "variant_id": 2,
-        "quantity": 1,
-        "name": "Niagara Falls poster",
-        "retail_price": "19.99",
-        "files": [{
-            "url": "http://example.com/files/posters/poster_2.jpg"
-        }]
-    }, {
-        "variant_id": 1,
-        "quantity": 3,
-        "name": "Grand Canyon poster",
-        "retail_price": "17.99",
-        "files": [{
-            "url": "http://example.com/files/posters/poster_3.jpg"
-        }]
-    }],
-    "retail_costs": {
-        "shipping": "24.50"
-    }
-}
-
-
-*/
 
 
 //@TODO: Twitter webhook, fire this when a new tweet is sent.
