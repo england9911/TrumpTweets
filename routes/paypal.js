312321527e81f5bdc1268ebaf2cf8412d4fac2cd
Matt England <matt.england@wearehalo.co.uk>
2017-11-16 13:06:56 +0000

    Console log paypal error

--- routes/paypal.js
+++ routes/paypal.js
@@ -22,7 +22,7 @@ router.get('/checkout_return', function (req, res, next){
     // GMT express deprecated req.param(name): Use req.params, req.body, or req.query instead
     // at routes/paypal.js:20:23
 
-    var payerId = req.params('PayerID');
+    var payerId = req.param('PayerID');
 
     console.log('Params type 1: ' + req.param('PayerID'));
     console.log('Params type 2: ' + req.params('PayerID'));
