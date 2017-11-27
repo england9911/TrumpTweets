575e0ce5d36a65fdea2b96062884f652051f1d73
Matt England <m.p.england@gmail.com>
2017-11-27 08:49:55 +0000

    Regen initial work

---
+++ libs/posters/generateMissing.js
@@ -0,0 +1,109 @@
+var posters = require('../posters/makePoster');
+var products = require('../products/insertProducts');
+
+var mongodb = require('mongodb');
+var async = require('async');
+var path = require('path');
+var common = require('../../routes/common');
+var config = common.getConfig();
+var AWS = require('aws-sdk')
+
+
+// Load products.
+
+// Check the value of productImage. If it is NULL or "/uploads/placeholder.png" regenerate the poster & thumbnail.
+
+// Save the new thumbnail value on AWS to the productImage field.
+
+
+// It's generally just the thumbnail that has not been created. Not the full poster.
+function reCreate(callback) {
+
+	mongodb.connect(config.databaseConnectionString, {}, function(err, db) {
+
+		if(err) {
+			console.log(err);
+			return;
+		}
+
+		var productsCol = db.collection('products');
+
+		var cursor = productsCol.find(
+		   {
+		      productImage: { $in: [ null,  '/uploads/placeholder.png' ] }
+		   }
+		).project({ productImage: 1, productPermalink: 1, productTitle: 1 });
+
+		cursor.toArray(function(err, results) {
+
+			if (err) throw err;
+
+			console.log('results:');
+			console.log(results);
+
+			// Run ONE regeneration at a time to save memory.
+
+	        if(results.length > 0) {
+
+	            async.eachSeries(files, function(thumb, callback3) {
+
+	                // Regenerate the thumbnail (Lambda function). function = posterThumb.
+	                var lambda = new AWS.Lambda();
+
+	                // SEE: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Lambda.html#invoke-property
+
+	                // Thought: Check the lambda function itself to see if there are any weird characters in the filename
+					
+					var params = {
+					  FunctionName: "posterThumb", 
+					  LogType: "None", 
+					  Payload: '<JSON that you want to provide to your Lambda function as input>', 
+					  Qualifier: "1"
+					};
+					
+					lambda.invoke(params, function(err, data) {
+					   if (err) console.log(err, err.stack); // an error occurred
+					   else     console.log(data);           // successful response
+					   /*
+					   data = {
+					    FunctionError: "", 
+					    LogResult: "", 
+					    Payload: <Binary String>, 
+					    StatusCode: 123
+					   }
+					   */
+					});
+
+	                // Save the thumbnail into the products collection.
+	            });
+	        }	
+	        else {
+	            return callback(false);
+	        }
+
+
+
+
+	    });
+
+		
+
+	});
+
+};
+
+reCreate();
+
+
+
+
+// tweets.loadTweets(function(errLoad, tweets) {
+
+// 	if(errLoad) {
+// 		console.log(errLoad);
+// 		return;
+// 	}
+
+
+
+// });
